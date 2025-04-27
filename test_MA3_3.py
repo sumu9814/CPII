b4d3219933f974add85006120d19bb0440e01930
Supraja Muralidharan <sumu9814@arrhenius.it.uu.se>
2025-04-27 23:09:08 +0200

    MA3 - CP II

--- test_MA3_3.py
+++ test_MA3_3.py
@@ -24,3 +24,6 @@ class Test(unittest.TestCase):
         self.assertLess(1.6, app_vol)
         self.assertLess(app_vol, 2.2)
         self.assertLess(par, seq)
+
+if __name__ == "__main__":
+    unittest.main()
